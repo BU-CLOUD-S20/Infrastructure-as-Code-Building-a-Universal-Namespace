386740ea599343cd2c31d94605173b6fd0e427d8
ZeyuKeithFu <zeyufu@bu.edu>
2020-02-22 17:58:47 -0500

    Migrate shell script into playbooks.

--- upspin/build.sh
+++ upspin/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 ## Read input options
 while getopts ":d:e:i:z:t:" opt; do
   case ${opt} in
@@ -19,10 +18,6 @@ while getopts ":d:e:i:z:t:" opt; do
   esac
 done
 
-### Download Upspin packages
-#sudo wget https://upspin.io/dl/upspin.darwin_amd64.tar.gz
-#sudo tar -C /usr/local/bin -xzf upspin.darwin_amd64.tar.gz
-
 ## Sign up new Upspin user
 upspin signup -server=${domain} ${email}
 upspin setupdomain -domain=${domain} > records.txt
@@ -37,16 +32,6 @@ arr[$index]=${i}
 let "index+=1"
 done
 txt_record=${arr[3]}
-#rm -f records.txt
-
-## Create TXT record
-p1='{"type":"TXT","name":"'
-p2='","content":"'
-p3='","ttl":900,"priority":10,"proxied":false}'
-curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone}/dns_records" \
--H "Authorization: Bearer ${token}" \
--H "Content-Type:application/json" \
---data ${p1}${domain}${p2}${txt_record}${p3}
 
 ## Create A record
 p1='{"type":"A","name":"'
@@ -57,5 +42,11 @@ curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone}/dns_records" \
 -H "Content-Type:application/json" \
 --data ${p1}${domain}${p2}${ip}${p3}
 
-## Set up server
-upspin setupserver --host=${domain} --domain=${domain}
+## Create TXT record
+p1='{"type":"TXT","name":"'
+p2='","content":"'
+p3='","ttl":900,"priority":10,"proxied":false}'
+curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone}/dns_records" \
+-H "Authorization: Bearer ${token}" \
+-H "Content-Type:application/json" \
+--data ${p1}${domain}${p2}${txt_record}${p3}
