716de4efa5bb30bbaf5ca30e066ecf2e34f464a4
ZeyuKeithFu <zeyufu@bu.edu>
2020-02-22 16:32:48 -0500

    Add build.yml

--- upspin/build_local.sh
+++ upspin/build_local.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
-## Download Upspin packages
-sudo wget https://upspin.io/dl/upspin.darwin_amd64.tar.gz
-sudo tar -C /usr/local/bin -xzf upspin.darwin_amd64.tar.gz
+
+## Read input options
+while getopts ":d:e:i:z:t:" opt; do
+  case ${opt} in
+    d) domain="$OPTARG"
+    ;;
+    e) email="$OPTARG"
+    ;;
+    i) ip="$OPTARG"
+    ;;
+    z) zone="$OPTARG"
+    ;;
+    t) token="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+### Download Upspin packages
+#sudo wget https://upspin.io/dl/upspin.darwin_amd64.tar.gz
+#sudo tar -C /usr/local/bin -xzf upspin.darwin_amd64.tar.gz
 
 ## Sign up new Upspin user
-echo "Please enter your Upspin domain:"
-read domain
-echo "Please enter your email address:"
-read email
 upspin signup -server=${domain} ${email}
 upspin setupdomain -domain=${domain} > records.txt
 
@@ -22,31 +37,25 @@ arr[$index]=${i}
 let "index+=1"
 done
 txt_record=${arr[3]}
-rm -f records.txt
+#rm -f records.txt
 
-## Create sub-domain
-echo "Please enter you DNS record zone ID:"
-read zone_id
-echo "Please enter your api token"
-read token
-echo "Please enter your ip address:"
-read ip_address
-p1='{"type":"A","name":"'
-p2='","content":"'
-p3='","ttl":120,"priority":10,"proxied":false}'
-curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records" \
--H "Authorization: Bearer ${token}" \
--H "Content-Type:application/json" \
---data ${p1}${domain}${p2}${ip_address}${p3}
-
-## Create DNS record
+## Create TXT record
 p1='{"type":"TXT","name":"'
 p2='","content":"'
 p3='","ttl":900,"priority":10,"proxied":false}'
-curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records" \
+curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone}/dns_records" \
 -H "Authorization: Bearer ${token}" \
 -H "Content-Type:application/json" \
+--data ${p1}${domain}${p2}${txt_record}${p3}
 
-## Build runnable binary
-GOOS=linux GOARCH=amd64 go build upspin.io/cmd/upspinserver
-scp -i ~/.ssh/id_rsa upspinserver upspin@${domain}:upspinserver
+## Create A record
+p1='{"type":"A","name":"'
+p2='","content":"'
+p3='","ttl":120,"priority":10,"proxied":false}'
+curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone}/dns_records" \
+-H "Authorization: Bearer ${token}" \
+-H "Content-Type:application/json" \
+--data ${p1}${domain}${p2}${ip}${p3}
+
+## Set up server
+upspin setupserver --host=${domain} --domain=${domain}
